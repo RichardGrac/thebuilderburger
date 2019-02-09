4b76f3d87ed519a1f239fc89a4a0c5a2473d9518
Ricardo Garcia <Richard_Grac@hotmail.com>
2019-02-09 12:43:17 -0600

    Adding async components (Lazy loading)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, {Component, Suspense} from 'react'
-import Layout from "./containers/Layout"
-import BuilderBurger from "./containers/BurgerBuilder"
-import Checkout from './containers/Checkout/Checkout'
+import React, {Component, Suspense} from 'react';
+import Layout from "./containers/Layout";
+import BuilderBurger from "./containers/BurgerBuilder";
 import {withRouter, Route, Switch} from 'react-router-dom'
 import {CHECKOUT_URL, HOME_URL, LOG_OUT, ORDERS_URL, SIGN_IN, SIGN_UP} from './utilities/constants'
 import Auth from './containers/Auth'
 import {Redirect} from 'react-router'
 import {connect} from 'react-redux'
 import {authCheckState, logout} from './redux/Authentication/actions/auth'
+
 const Orders = React.lazy(() => import('./containers/Orders/Orders'))
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'))
 
 class App extends Component {
 
     componentDidMount() {
         this.props.onTryToLogin()
+    }
+
+    asyncComponent = (component) => {
+        return (
+            <Suspense fallback={<div>Loading...</div>}>
+                {(component)}
+            </Suspense>
+        )
     }
 
     logout = () => {
@@ -28,10 +37,8 @@ class App extends Component {
                 {isLogged ? (
                     <Switch>
                         <Route path={HOME_URL} exact component={BuilderBurger} />
-                        <Route path={CHECKOUT_URL} component={Checkout} />
-                        <Suspense fallback={<div>Wait please...</div>}>
-                            <Route path={ORDERS_URL} component={Orders} />
-                        </Suspense>
+                        <Route path={CHECKOUT_URL} render={() => this.asyncComponent(<Checkout />)} />
+                        <Route path={ORDERS_URL} component={() => this.asyncComponent(<Orders />)} />
                         <Route path={LOG_OUT} render={this.logout} />
                         <Redirect to={HOME_URL} />
                     </Switch>
@@ -44,7 +51,7 @@ class App extends Component {
                     </Switch>
                 )}
             </Layout>
-        )
+        );
     }
 }
 
